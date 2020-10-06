387aed6aefcf5e8b3f5238d84a7f76eaac10d0fd
Uriel Medina <urimeba511@gmail.com>
2020-10-06 14:19:44 -0500

    *fix* Checking if autor with same names already exits

--- Apps/Users/forms.py
+++ Apps/Users/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from .models import Contrato, Facultad, Nivel, LineaInvestigacion, User, Articulo,  CapituloLibro, Patente, Congreso, Investigacion, Tesis, Autor
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
-from django.utils.safestring import mark_safe
+from django.core.exceptions import ValidationError
 
 class UserActualizadoForm(forms.Form):
     error_css_class = "error"
@@ -82,6 +82,16 @@ class AutorForm(ModelForm):
     class Meta:
         model = Autor
         fields = ['first_name', 'last_name']
+
+    def clean(self):
+        cleaned_data=super(AutorForm, self).clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        autor_existente = Autor.objects.filter(first_name=first_name, last_name=last_name).count()
+        if autor_existente == 0:
+            raise ValidationError('Un autor con los mismos datos ya existe. Eligelo o verifica tus datos')
+
+
 
 
 class CapituloLibroForm(ModelForm):
