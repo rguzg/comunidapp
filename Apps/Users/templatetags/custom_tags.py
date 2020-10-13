aca20f51c7973b58d294154e47cc74483c80d6ef
Uriel Medina <urimeba511@gmail.com>
2020-10-12 21:17:39 -0500

    *refactor* Refactor URLs, Button name, Template tags, Validators, Errors

--- Apps/Users/templatetags/custom_tags.py
+++ Apps/Users/templatetags/custom_tags.py
@@ -1,11 +1,8 @@
-import ast
 from django import template
 register = template.Library()
-
-@register.filter
-def make_dict(val):
-    return ast.literal_eval(val)
 
 @register.filter(name='addclass')
 def addclass(value, arg):
     return value.as_widget(attrs={'class': arg})
+
+                   
--- Apps/Users/validators.py
+++ Apps/Users/validators.py
@@ -1,10 +1,8 @@
 from django.core.validators import RegexValidator
-
-'''
-This regex assumes that you have a clean string,
-you should clean the string for spaces and other characters
-'''
+from django import template
+register = template.Library()
 
 isalphavalidator = RegexValidator(r'^[a-zA-Z ]+$',
                              message='El nombre debe ser alfanumerico',
                              code='Nombre no válido')
+  
