998abd76da0ffd0d99a8f84df4311f70340e95e3
Uriel Medina <urimeba511@gmail.com>
2020-10-12 21:22:22 -0500

    *fix* Passing args to Popup Window at JS

--- Static/js/functions.js
+++ Static/js/functions.js
@@ -5,14 +5,20 @@ function showEditPopup(url) {
 }
 
 function showAddPopup(triggeringLink) {
+    const id = triggeringLink;
+    // console.log(id.id);
+    const sibilingId = id.previousElementSibling.id;
+    // console.log(sibilingId);
     const name = triggeringLink.id.replace(/^add_/, '');
     const href = triggeringLink.getAttribute('data-url');
     const win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
+    localStorage.setItem('id_field', sibilingId);
     win.focus();
     return false;
 }
 
 function closePopup(win, newID, newRepr, id) {
+    // console.log(win, newID, newRepr, id);
     let select = document.getElementById(id);
     let option = document.createElement('option');
     option.appendChild(document.createTextNode(newRepr));
@@ -20,4 +26,6 @@ function closePopup(win, newID, newRepr, id) {
     option.setAttribute('selected', 'selected');
     select.appendChild(option);
     win.close();
+
+    localStorage.removeItem('id');
 }
