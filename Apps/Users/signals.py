076dccd14395a3848dcd1294f8376a1dbb7e46b8
Uriel Medina <urimeba511@gmail.com>
2020-11-09 20:13:55 -0600

    *fix* Signal for creating the UpdateRequest at creating user

--- Apps/Users/signals.py
+++ Apps/Users/signals.py
@@ -1,10 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from Apps.Users.models import User, Autor
+from Apps.Users.models import User, Autor, UpdateRequest
 
 # Señal que crea un Autor de manera automatica cada vez que es creado un nuevo usuario
 @receiver(post_save, sender=get_user_model())
 def create_user_autor(sender, instance, created, **kwargs):
     if created:
         Autor.objects.create(user=instance)
+        UpdateRequest.objects.create(
+            user=instance,
+            estado='P'
+        )
+
