83d1edf21e038b616c6284d6ac644e0ded2d2804
Uriel Medina <urimeba511@gmail.com>
2020-12-02 11:56:55 -0600

    *fix* Unncomented getCookie functions on JS file

--- Static/js/functions.js
+++ Static/js/functions.js
@@ -28,21 +28,21 @@ window.onload = function () {
     inputSearch.onkeyup = searchUsers(inputSearch);
 }
 
-// function getCookie(name) {
-//     var cookieValue = null;
-//     if (document.cookie && document.cookie != '') {
-//         var cookies = document.cookie.split(';');
-//         for (var i = 0; i < cookies.length; i++) {
-//             var cookie = cookies[i].trim();
-//             // Does this cookie string begin with the name we want?
-//             if (cookie.substring(0, name.length + 1) == (name + '=')) {
-//                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-//                 break;
-//             }
-//         }
-//     }
-//     return cookieValue;
-// }
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 // function showEditPopup(url) {
 //     var win = window.open(url, "Edit",
