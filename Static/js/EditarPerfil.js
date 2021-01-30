1bcfd15e3aeaa21da9d329d2ec8e5c5a1bad2c36
Raúl <raulguzmanguerrero@outlook.com>
2021-01-29 18:09:39 -0600

    Add pill_input to Editar Perfil

---
+++ Static/js/EditarPerfil.js
@@ -0,0 +1,8 @@
+const lineas_investigacion = document.querySelector('#pills_lineas');
+PillsBox(lineas_investigacion, 'lineas');
+
+const niveles = document.querySelector('#pills_niveles');
+PillsBox(niveles, 'niveles');
+
+const facultades = document.querySelector('#pills_facultades');
+PillsBox(facultades, 'facultades');
