f39c5ce8f92806351abfb29605d63d665b5a2124
Raúl <raulguzmanguerrero@outlook.com>
2021-02-02 17:33:51 -0600

    Add id to perfil-detail form

---
+++ Static/js/EditarPerfil.js
@@ -0,0 +1,14 @@
+const lineas_investigacion = document.querySelector('#pills_lineas');
+PillsBox(lineas_investigacion, 'lineas');
+
+const niveles = document.querySelector('#pills_niveles');
+PillsBox(niveles, 'niveles');
+
+const facultades = document.querySelector('#pills_facultades');
+PillsBox(facultades, 'facultades');
+
+const form = document.querySelector('#perfilForm')
+
+form.addEventListener('submit', () => {
+    alert();
+})
