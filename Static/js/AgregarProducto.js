4a5e12cc91e735355625ed2c7f3d73e659a1f228
Raúl <raulguzmanguerrero@outlook.com>
2021-02-06 23:33:41 -0600

    Add EventListener to send form data to /proxy

--- Static/js/AgregarProducto.js
+++ Static/js/AgregarProducto.js
@@ -3,3 +3,48 @@ PillsBox(palabras_clave, 'palabras');
 
 const lineas_investigacion = document.querySelector('#pills_lineas');
 PillsBox(lineas_investigacion, 'lineas');
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    let data = CreateFormData(form);
+
+        // Añadir los valores de los PillsBox
+        let pills_lineas = lineas_investigacion.querySelectorAll('.m-pills');
+        let lineas_array = [];
+    
+        pills_lineas.forEach(element => {
+            lineas_array.push(JSON.stringify({
+                id: element.dataset.id,
+                nombre: element.firstChild.textContent
+             }));
+        });
+        data.append("lineas", lineas_array);
+    
+        // El pill_input de niveles no está en todos los forms de AgregarProducto
+        if(palabras_clave){
+            let pills_palabras = palabras_clave.querySelectorAll('.m-pills');
+            let palabras_array = [];
+        
+            pills_palabras.forEach(element => {
+               palabras_array.push(JSON.stringify({
+                   id: element.dataset.id,
+                   nombre: element.firstChild.textContent
+                }));
+            });
+            data.append("palabras", palabras_array);
+        }
+
+        try {
+            await fetch('/proxy', {
+                method: 'POST',
+                body: data
+            }).then(() => {
+                // location.reload();
+            })
+        } catch (error) {
+            console.error("Error mandando la actualización de perfil");
+        }
+})
