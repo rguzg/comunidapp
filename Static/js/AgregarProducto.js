4c699bccf07965a1453766e39a0c749b1861fa93
Raúl <raulguzmanguerrero@outlook.com>
2021-02-08 17:18:45 -0600

    Add check if palabras_clave input_pills exists

--- Static/js/AgregarProducto.js
+++ Static/js/AgregarProducto.js
@@ -1,5 +1,8 @@
 const palabras_clave = document.querySelector('#pills_palabras');
-PillsBox(palabras_clave, 'palabras');
+
+if(palabras_clave){
+    PillsBox(palabras_clave, 'palabras');
+}
 
 const lineas_investigacion = document.querySelector('#pills_lineas');
 PillsBox(lineas_investigacion, 'lineas');
