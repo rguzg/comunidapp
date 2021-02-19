bc6125f408a16078bfff7ee3878354ad582783c9
Raúl <raulguzmanguerrero@outlook.com>
2021-02-19 12:34:23 -0600

    Add VerificarCambiosForm to EditarPerfil

--- Static/js/EditarPerfil.js
+++ Static/js/EditarPerfil.js
@@ -1,3 +1,47 @@
+/*
+    Esta función agrega o quita el atributo disabled de boton_submit dependiendo de si los valores del form han cambiado o no
+*/
+
+const VerificarCambiosForm = (form, boton_submit) => {
+    if(form instanceof HTMLFormElement){
+        let original_values = {};
+        let current_values = {};
+
+        // Verificación de los valores que son inputs
+        for (let i = 0; i < form.elements.length; i++){
+            let element = form.elements[i];
+        
+            if( element.type != "submit" && element.type != "button" 
+            && element.name != "csrfmiddlewaretoken" && element.name != "user"){
+                element.addEventListener('input', () => {
+                    current_values[element.name] = element.value;
+                    
+                    for (const key in original_values) {
+                        if(original_values[key] != current_values[key]){
+                            // Como el valor del input se agrega a current_values cuando su valor cambia, current_value[key] puede ser
+                            // igual a undefined si el valor todavía no ha cambiado
+                            if(!(current_values[key] == undefined)){
+                                boton_submit.removeAttribute('disabled');
+                                break;
+                            }
+                        } else {
+                            boton_submit.setAttribute('disabled', '');
+                        }
+                    }
+
+                });
+        
+                original_values[element.name] = element.value;
+            }
+
+        }
+
+        // Verificación que no hayan cambiado las pills que no agregó el usuario
+    } else {
+        throw new TypeError("form debe ser un HTMLFormElement")
+    }
+}
+
 const lineas_investigacion = document.querySelector('#pills_lineas');
 PillsBox(lineas_investigacion, 'lineas');
 
@@ -8,6 +52,9 @@ const facultades = document.querySelector('#pills_facultades');
 PillsBox(facultades, 'facultades');
 
 const form = document.querySelector('#perfilForm');
+const boton_submit = form.querySelector('input[type="submit"]');
+
+VerificarCambiosForm(form, boton_submit)
 
 /* Para poder enviar los datos que hay en los PillsBox.
 JavaScript se va a encargar de crear el formdata que se le va a enviar al servidor. */
