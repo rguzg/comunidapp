985062cf16b6dac6c8a6586f84245b02dccd05c5
Raúl <raulguzmanguerrero@outlook.com>
2021-04-27 19:24:55 -0500

    Add Users_Serializer and Relaciones_Serializer

--- Apps/Users/serializers.py
+++ Apps/Users/serializers.py
@@ -1,4 +1,5 @@
-from .models import LineaInvestigacion, Facultad, Nivel, PalabrasClave
+from .models import Relaciones_Profesores, User, LineaInvestigacion, Facultad, Nivel, PalabrasClave
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 class LineasInvestigacion_Serializer(ModelSerializer):
@@ -20,3 +21,18 @@ class PalabrasClave_Serializer(ModelSerializer):
     class Meta:
         model = PalabrasClave
         fields = '__all__'
+
+class Users_Serializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username'] 
+        read_only_fields = ['id', 'first_name', 'last_name', 'username'] 
+
+class Relaciones_Serializer(ModelSerializer):
+    source = serializers.IntegerField(source = 'profesor1.user.pk', )
+    target = serializers.IntegerField(source = 'profesor2.user.pk')
+    
+    class Meta:
+        model = Relaciones_Profesores
+        fields = ['source', 'target']
+        read_only_fields = ['source', 'targer']
