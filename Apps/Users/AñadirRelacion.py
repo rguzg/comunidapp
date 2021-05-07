fffd22e5e0d9838ee1c3c771f46379f98eb8ebc7
Raúl <raulguzmanguerrero@outlook.com>
2021-05-07 18:19:44 -0500

    Add Tesis to AñadirRelacion.py

--- Apps/Users/AñadirRelacion.py
+++ Apps/Users/AñadirRelacion.py
@@ -1,21 +1,24 @@
 # Esta función recibe algún producto y lo analiza para agregar las relaciones que esté creando al modelo
 # Relaciones_Profesores
-from .models import Autor, Relaciones_Profesores, Articulo, CapituloLibro, Patente, Congreso, Investigacion
+
+# Las relaciones se crean automaticamente, según lo indicado en signals.py
+from .models import Autor, Relaciones_Profesores, Articulo, CapituloLibro, Patente, Congreso, Investigacion, Tesis
 from typing import Union
 from django.db.utils import IntegrityError
 
-def CrearRelacion(producto: Union[Articulo, CapituloLibro, Patente, Congreso, Investigacion], profesor1: Autor, profesor2: Autor = None):
+def CrearRelacion(producto: Union[Articulo, CapituloLibro, Patente, Congreso, Investigacion, Tesis], profesor1: Autor, profesor2: Autor = None):
     switch_productos = {
         "Articulo": lambda : Relaciones_Profesores.objects.create(profesor1 = profesor1, profesor2 = profesor2, tipo_producto = Relaciones_Profesores.ARTICULO, articulo = producto),
         "Capitulo/Libro": lambda : Relaciones_Profesores.objects.create(profesor1 = profesor1, profesor2 = profesor2, tipo_producto = Relaciones_Profesores.CAPITULO_LIBRO, capituloLibro = producto),
         "Patente": lambda : Relaciones_Profesores.objects.create(profesor1 = profesor1, profesor2 = profesor2, tipo_producto = Relaciones_Profesores.PATENTE, patente = producto),
         "Congreso": lambda : Relaciones_Profesores.objects.create(profesor1 = profesor1, profesor2 = profesor2, tipo_producto = Relaciones_Profesores.CONGRESO, congreso = producto),
         "Investigacion": lambda : Relaciones_Profesores.objects.create(profesor1 = profesor1, profesor2 = profesor2, tipo_producto = Relaciones_Profesores.INVESTIGACION, investigacion = producto),
+        "Tesis": lambda : Relaciones_Profesores.objects.create(profesor1 = profesor1, profesor2 = profesor2, tipo_producto = Relaciones_Profesores.TESIS, tesis = producto),
     }
 
     switch_productos[producto.TipoProducto]()
 
-def AñadirRelacion(producto: Union[Articulo, CapituloLibro, Patente, Congreso, Investigacion]) -> None:
+def AñadirRelacion(producto: Union[Articulo, CapituloLibro, Patente, Congreso, Investigacion, Tesis]) -> None:
     # A diferencia de los demas modelos, la propiedad autores es diferente en el modelo Patente
     autores = None
 
