5c274a8175c5925cec0be4d1aa7dfe0f5e6021a7
Raúl <raulguzmanguerrero@outlook.com>
2021-05-18 16:14:35 -0500

    Add Autor_Serializer

--- Apps/Users/serializers.py
+++ Apps/Users/serializers.py
@@ -1,4 +1,4 @@
-from .models import Relaciones_Profesores, User, LineaInvestigacion, Facultad, Nivel, PalabrasClave
+from .models import Autor, Relaciones_Profesores, User, LineaInvestigacion, Facultad, Nivel, PalabrasClave
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -22,15 +22,22 @@ class PalabrasClave_Serializer(ModelSerializer):
         model = PalabrasClave
         fields = '__all__'
 
+# Este serializer estaba siendo utilizado para los JSON para los grafos, pero fue descontinuado a favor del serializador de autores ya que no todos los autores tienen usuarios
 class Users_Serializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'username'] 
         read_only_fields = ['id', 'first_name', 'last_name', 'username'] 
 
+class Autor_Serializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = ['id', 'first_name', 'last_name', 'user'] 
+        read_only_fields = ['id', 'first_name', 'last_name', 'user'] 
+
 class Relaciones_Serializer(ModelSerializer):
-    source = serializers.IntegerField(source = 'profesor1.user.pk', )
-    target = serializers.IntegerField(source = 'profesor2.user.pk')
+    source = serializers.IntegerField(source = 'profesor1.pk')
+    target = serializers.IntegerField(source = 'profesor2.pk', allow_null = True)
     
     class Meta:
         model = Relaciones_Profesores
--- Apps/Users/views_relaciones.py
+++ Apps/Users/views_relaciones.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView, exception_handler
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from .models import Articulo, Relaciones_Profesores
-from .serializers import Users_Serializer, Relaciones_Serializer
+from .serializers import Autor_Serializer, Users_Serializer, Relaciones_Serializer
 from django.views import View
 from django.shortcuts import HttpResponse, redirect, render
 
@@ -21,6 +21,7 @@ class Relaciones(APIView):
             "patentes": lambda :  Relaciones_Profesores.objects.exclude(patente = None),
             "congresos": lambda :  Relaciones_Profesores.objects.exclude(congreso = None),
             "investigaciones": lambda :  Relaciones_Profesores.objects.exclude(investigacion = None),
+            "tesis": lambda :  Relaciones_Profesores.objects.exclude(tesis = None),
         }
 
         resultado = {
@@ -34,15 +35,21 @@ class Relaciones(APIView):
             raise NotFound(f'{e} no es un producto valido', 404)
         
         for relacion in relaciones:
-            profesor1 = Users_Serializer(relacion.profesor1.user).data
+            profesor1 = Autor_Serializer(relacion.profesor1).data
             if(profesor1 not in resultado['nodes']):
                 resultado['nodes'].append(profesor1)
             if(relacion.profesor2):
-                profesor2 = Users_Serializer(relacion.profesor2.user).data
+                profesor2 = Autor_Serializer(relacion.profesor2).data
                 if(profesor2 not in resultado['nodes']):
                     resultado['nodes'].append(profesor2)
 
-                resultado['edges'].append(Relaciones_Serializer(relacion).data)
+            relacion_serializada = Relaciones_Serializer(relacion).data
+
+            # Esto se podria hacer con defaults de serializers, pero por el momento, es más rápido así
+            if(relacion_serializada['target'] == None):
+                relacion_serializada['target'] = relacion_serializada['source']
+
+            resultado['edges'].append(relacion_serializada)
         return Response(resultado)
 
 class Visualization(View):
