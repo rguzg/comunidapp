b865975f3b114b0bfbe5f6344a52a8b8edc62a5e
Raúl <raulguzmanguerrero@outlook.com>
2021-07-05 18:45:35 -0500

    Hide Estados whose data-pais isn't the currently selected pais and hide ciudades whose data-estado  isn't the currently selected estado

--- Static/js/AgregarProducto.js
+++ Static/js/AgregarProducto.js
@@ -1,4 +1,7 @@
 const palabras_clave_contenedor = document.querySelector('#pills_palabras');
+const paises_select = document.querySelector('#id_pais');
+const estadosP_select = document.querySelector('#id_estadoP');
+const ciudades_select = document.querySelector('#id_ciudad');
 
 let palabras_clave = null;
 
@@ -13,6 +16,40 @@ const boton_submit = form.querySelector('input[type="submit"]');
 
 const notification_controller = new NotificationController('bottom-right');
 
+/**
+ * Esconde los estados cuyos atributo data-pais no sea pais
+ * @param {String} pais 
+ */
+const HideEstados = (pais) => {
+    let estados = estadosP_select.options;
+
+    // Se empieza con el segundo elemento de estados porque el primero es el placeholder cuando nada está seleccionado
+    for (let i = 1; i < estados.length; i++) {
+        if(estados[i].dataset.pais == pais){
+            estados[i].classList.remove('h-display-none');
+        } else {
+            estados[i].classList.add('h-display-none');
+        }
+    }
+}
+
+/**
+ * Esconde las ciudades cuyos atributo data-pais no sea estado
+ * @param {String} estado 
+ */
+const HideCiudades = (estado) => {
+    let ciudades = ciudades_select.options;
+
+    // Se empieza con el segundo elemento de estados porque el primero es el placeholder cuando nada está seleccionado
+    for (let i = 1; i < ciudades.length; i++) {
+        if(ciudades[i].dataset.estado == estado){
+            ciudades[i].classList.remove('h-display-none');
+        } else {
+            ciudades[i].classList.add('h-display-none');
+        }
+    }
+}
+
 VerificarForm(form, boton_submit, [palabras_clave, lineas_investigacion]);
 
 form.addEventListener('submit', async (event) => {
@@ -23,7 +60,6 @@ form.addEventListener('submit', async (event) => {
 
     setTimeout(() => {
         boton_submit.removeAttribute('disabled', '');
-        console.log("A");
     }, 50000);
 
     let data = CreateFormData(form);
@@ -62,3 +98,34 @@ form.addEventListener('submit', async (event) => {
         console.error(`Error agregando producto ${error}`);
     }
 })
+
+if(estadosP_select && ciudades_select){
+    // Esconder todas los estados y ciudades
+    HideEstados("");
+    HideCiudades("");
+
+    paises_select.addEventListener('input', () => {
+        let selected_pais_nombre = paises_select.options[paises_select.selectedIndex].innerText;
+        let selected_estadoP = estadosP_select.options[estadosP_select.selectedIndex]
+        
+        HideEstados(selected_pais_nombre);
+
+        // Quitar el estado seleccionado actual si ese estado no existe en el nuevo pais seleccionado 
+        if(selected_estadoP.dataset.pais != selected_pais_nombre){
+            estadosP_select.value = "";
+        } 
+        estadosP_select.dispatchEvent(new Event('input'));
+    });
+
+    estadosP_select.addEventListener('input', () => {
+        let selected_estado_nombre = estadosP_select.options[estadosP_select.selectedIndex].innerText;
+        let selected_ciudad = ciudades_select.options[ciudades_select.selectedIndex]
+        
+        HideCiudades(selected_estado_nombre);
+
+        // Quitar la ciudad seleccionado actual si esa ciudad no existe en el nuevo estado seleccionado 
+        if(selected_ciudad.dataset.estado != selected_estado_nombre){
+            ciudades_select.value = "";
+        } 
+    });
+}
