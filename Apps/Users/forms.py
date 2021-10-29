04d5efb06e233147504c691ef75424b4cda0c4cc
Raúl <raulguzmanguerrero@outlook.com>
2021-10-28 19:27:53 -0500

    Hotfix: Add None guard on ArticuloForm palabras_clave check
    Before the line only checked for palabras_clave.count() < 3 and well that wouldn't work if palabras_clave was None

--- Apps/Users/forms.py
+++ Apps/Users/forms.py
@@ -292,7 +292,7 @@ class ArticuloForm(ModelForm):
                     'publicacion', 'Debes agregar una fecha de publicación')
 
         palabras_clave = cleaned_data.get('palabras_clave')
-        if palabras_clave.count() < 3:
+        if not palabras_clave or palabras_clave.count() < 3:
             self.add_error('palabras_clave',
                            'Debes escoger al menos 3 palabras clave')
 
