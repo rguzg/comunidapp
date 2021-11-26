1fa2a3afc9d49828abe55657d1ee9dfdac5a353d
Raúl <53493295+rguzg@users.noreply.github.com>
2021-11-26 13:18:33 -0600

    Feat(ModalesAgregar): Add estadoP/create endpint
    This endpoint is used by the Create EstadoP popup

--- Apps/Users/urls.py
+++ Apps/Users/urls.py
@@ -40,6 +40,8 @@ urlpatterns = [
     url(r'^facultad/create', login_required(views.FacultadCreatePopup.as_view()), name = "FacultadCreate"),
     url(r'^nivel/create', login_required(views.NivelesCreatePopup.as_view()), name = "NivelCreate"),
     url(r'^contrato/create', login_required(views.ContratoCreatePopup.as_view()), name = "ContratoCreate"),
+    url(r'^estadoP/create', login_required(views.EstadoPCreatePopup.as_view()),
+        name="EstadoPCreate"),
 
     #URLs para realizar busquedas
     path('buscar/lineas', login_required(views_busqueda.BuscarLineas.as_view()), name="BuscarLineas"),
