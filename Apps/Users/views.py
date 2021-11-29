9fbb3854347f6c74087500caedd5a2ef4b199c96
Raúl <53493295+rguzg@users.noreply.github.com>
2021-11-29 10:48:16 -0600

    Feat(ModalesAgregar): Change content of data.name in EditorialCreatePopup.post
    Changed from instance.nombre to instance.tipo

--- Apps/Users/views.py
+++ Apps/Users/views.py
@@ -1034,7 +1034,13 @@ class EditorialCreatePopup(View):
         if form.is_valid():
             id_field=form.cleaned_data.get('id_field')
             instance=form.save()
-            return HttpResponse('<script>opener.closePopup(window, "%s", "%s", "%s");</script>' % (instance.pk, instance, id_field))
+            data = {
+                "key": instance.pk,
+                "name": instance.nombre,
+                "targetID": id_field,
+            }
+
+            return HttpResponse(f"<script>opener.closePopup(window, JSON.parse('{json.dumps(data)}'));</script>")
         return render(request, "add-externo.html", {
             "form": form,
             'title': 'Agrega un tipo de Editorial'
