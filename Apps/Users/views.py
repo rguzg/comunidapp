f79fa51b42eb6ab6a16ae7564b2afab1cd571972
Raúl <53493295+rguzg@users.noreply.github.com>
2021-11-28 23:20:17 -0600

    Feat(ModalesAgregar): Change data format return by  AlumnoCreatePopup.post
    This change is inline with the new format of the closePopup function. A JSON is now returned.

--- Apps/Users/views.py
+++ Apps/Users/views.py
@@ -766,7 +766,13 @@ class AlumnoCreatePopup(View):
         if form.is_valid():
             id_field=form.cleaned_data.get('id_field')
             instance=form.save()
-            return HttpResponse('<script>opener.closePopup(window, "%s", "%s", "%s");</script>' % (instance.pk, instance, id_field))
+            data = {
+                "key": instance.pk,
+                "name": instance.expediente,
+                "targetID": id_field,
+            }
+
+            return HttpResponse(f"<script>opener.closePopup(window, JSON.parse('{json.dumps(data)}'));</script>")
         return render(request, 'add-externo.html', {
             'form': form,
             'title': 'Agrega un Alumno'
