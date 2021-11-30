945e3f38fb5626cbd6ac1e13b2d6d094a65aae3a
Raúl <53493295+rguzg@users.noreply.github.com>
2021-11-30 17:39:03 -0600

    feat(EverySerializer): Add Productos_Serializer_View
    This view displays all of the available:
    - Articulos
    - CapituloLibros
    - Patentes
    - Congresos
    - Investigaciones
    - Tesis

--- Apps/Users/views_serializer.py
+++ Apps/Users/views_serializer.py
@@ -1,6 +1,7 @@
-from .serializers import Facultad_Serializer, LineasInvestigacion_Serializer, Nivel_Serializer, PalabrasClave_Serializer
+from .serializers import Facultad_Serializer, LineasInvestigacion_Serializer, Nivel_Serializer, PalabrasClave_Serializer, Articulo_Serializer, CapituloLibro_Serializer, Patente_Serializer, Congreso_Serializer, Investigacion_Serializer, Tesis_Serializer
 from rest_framework import generics, permissions
-from .models import PalabrasClave
+from drf_multiple_model.views import ObjectMultipleModelAPIView
+from .models import PalabrasClave, Articulo, CapituloLibro, Patente, Congreso, Investigacion, Tesis
 
 class LineasInvestigacion_Serializer_View(generics.ListCreateAPIView):
     serializer_class = LineasInvestigacion_Serializer
@@ -28,3 +29,16 @@ class PalabraClave_Serializer_View(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = PalabrasClave.objects.all()
 
+
+class Productos_Serializer_View(ObjectMultipleModelAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    querylist = [
+        {'queryset': Articulo.objects.all(), 'serializer_class': Articulo_Serializer},
+        {'queryset': CapituloLibro.objects.all(
+        ), 'serializer_class': CapituloLibro_Serializer},
+        {'queryset': Patente.objects.all(), 'serializer_class': Patente_Serializer},
+        {'queryset': Congreso.objects.all(), 'serializer_class': Congreso_Serializer},
+        {'queryset': Investigacion.objects.all(
+        ), 'serializer_class': Investigacion_Serializer},
+        {'queryset': Tesis.objects.all(), 'serializer_class': Tesis_Serializer},
+    ]
